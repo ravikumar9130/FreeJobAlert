10420081dd81cd79d2bc727d1603b353d52b2acb
ajs_codes <sharanajs2001@gmail.com>
2021-08-24 12:29:10 +0530

    created header with navbar and added routes for different pages

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,32 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
-router.get('/job', function(req, res, next) {
-  res.render('jobDetails');
+// all india jobs
+router.get('/central-jobs', function(req, res, next) {
+  res.render('central-jobs');
+})
+router.get('/state-jobs', function(req, res, next) {
+  res.render('state-jobs');
+})
+router.get('/bank-jobs', function(req, res, next) {
+  res.render('bank-jobs');
+})
+router.get('/teaching-jobs', function(req, res, next) {
+  res.render('teaching-jobs');
+})
+router.get('/engineering-jobs', function(req, res, next) {
+  res.render('engineering-jobs');
+})
+router.get('/railway-jobs', function(req, res, next) {
+  res.render('railway-jobs');
+})
+router.get('/police-defence-jobs', function(req, res, next) {
+  res.render('police-defence-jobs');
+})
+router.get('/job-details', function(req, res, next) {
+  res.render('job-details');
 });
+
+
 
 module.exports = router;
