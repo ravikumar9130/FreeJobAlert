e46cbeabaf299c16aad8d4217bb49564428c832d
ravikumar <ravikumarwolfd9130@gmail.com>
2021-09-02 12:14:52 +0530

    sample commit

--- data/newsData.js
+++ data/newsData.js
@@ -69,6 +69,41 @@ const newsData = [
     
    
 },
+  {
+    Tittle:"NPCIL Trade Apprentice Online Form 2021",
+    Postdate: "25-08-2021",
+    // LatestUpdate: String,
+    TotalVacancy: 107,
+    BriefInformation: " Nuclear Power Corporation of India (NPCIL) has published notification for the recruitment of Trade Apprentice vacancy. Those Candidates who are interested in the vacancy details & completed all eligibility criteria can read the Notification & Apply Online.",
+    CompanyDetails: {
+        CompanyName:" Nuclear Power Corporation of India (NPCIL)" ,
+        AdvtNo: "Advt No: 01/2021",
+        VacancyName:"Trade Apprentice Vacancy 2021",
+    },
+    // ApplicationFee: {
+    //     fees:String
+    // },
+    ImportantDates: {
+      DatesInfo: [
+          
+    "Starting Date to Apply Online: 25-08-2021",
+    "Last Date to Apply Online: 13-09-2021",
+    "Last Date for Receipt of hard Copy of the Application: 27-09-2021"
+
+        ]
+    },
+    AgeLimit: {
+      LimitData: [
+          
+    "Minimum Age Limit: 14 Years",
+    "Maximum Age Limit: 24 Years",
+    "Age relaxation is admissible as per rules."
+
+        ]
+    }, Qualification: "Candidates should possess ITI (Relevant Discipline)",
+    
+   
+},
   ];
   
   module.exports = newsData;
